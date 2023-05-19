e0c9195313b63778d1c08e3fa260f925274e9058
MauMu21 <miaut16umf@gmail.com>
2023-05-19 00:51:45 -0400

    bad :-1:

---
+++ CAROUSELS/CAR1/index.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.carousel',{
+    navigation:{
+        nexEl: '.swiper/button-next',
+        prevEl: '.swiper/button-next'
+    },
+
+    pagination :{
+        el:'.swiper-pagination',
+    },
+})
